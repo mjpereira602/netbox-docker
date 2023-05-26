942b2761449cf54d557d58cae47b000acd9c842f
Michael Pereira <mpereira@bandwidth.com>
2023-05-26 13:20:06 -0400

    Add plugins to migrate from netbox-secretstore to netbox-secrets

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,10 +4,14 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-# PLUGINS = ["netbox_bgp"]
+PLUGINS = [
+  "netbox_plugin_extensions",
+  "netbox_secretstore",
+  "netbox_secrets"
+]
 
-# PLUGINS_CONFIG = {
-#   "netbox_bgp": {
-#     ADD YOUR SETTINGS HERE
-#   }
-# }
+PLUGINS_CONFIG = {
+  "netbox_plugin_extensions": {},
+  "netbox_secretstore": {},
+  "netbox_secrets": {}
+}
