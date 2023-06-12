db253dde023669e62433aa18cc553a9795fd05a9
Michael Pereira <mpereira@bandwidth.com>
2023-06-12 11:19:08 -0400

    Modify override files to enable automatic migration on demo box.

--- backup_restore_upgrade_netbox_voxbone_com.sh
+++ backup_restore_upgrade_netbox_voxbone_com.sh
@@ -4,6 +4,9 @@
 # As this script disappears as soon as it changes the containing repo's branch.
 #
 {
+  # Source variables
+  source .env
+
   #
   # Pull backup from netbox.voxbone.com
   #
@@ -29,73 +32,54 @@
   fi
     
   #
-  # Spool up a netbox 3.1 instance and restore netbox.voxbone.com into it.
-  #
-  podman-compose  down -v
-  podman volume prune -f
-  git checkout bandwidth-3.1-1.6.0
-  git pull --set-upstream origin bandwidth-3.1-1.6.0
-
-  podman-compose ${overrides} build 
-  podman-compose ${overrides} up --no-start
-  podman-compose ${overrides} start
-  
-  while ! curl -s -X GET http://localhost:8000 > /dev/null; do sleep 5; done
-  
-  #
-  # Run two extra migration scripts to prep for netbox-3.2+
-  #
-  curl -X GET "http://localhost:8000/api/extras/scripts/" \
-    -H "accept: application/json; indent=4" \
-    -H "Authorization: Token 0123456789abcdef0123456789abcdef01234567"
-    
-  curl -X POST "http://localhost:8000/api/extras/scripts/netbox_v32_migration.MigrateSiteASNsScript/" \
-    -H "accept: application/json; indent=4" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Token 0123456789abcdef0123456789abcdef01234567" \
-    --data '{ "data": { "clear_site_field": true }, "commit": true }'
-  
-  curl -X POST "http://localhost:8000/api/tenancy/contact-roles/" \
-    -H "accept: application/json; indent=4" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Token 0123456789abcdef0123456789abcdef01234567" \
-    --data '{ "name": "Site", "slug": "site", "description": "Site Contacts" }'
-  
-  curl -X POST "http://localhost:8000/api/extras/scripts/netbox_v32_migration.MigrateSiteContactsScript/" \
-    -H "accept: application/json; indent=4" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Token 0123456789abcdef0123456789abcdef01234567" \
-    --data '{ "data": { "clear_site_fields": true, "contact_priority": "" }, "commit": true }'
-  
-  curl -X GET "http://localhost:8000/api/extras/scripts/" \
-    -H "accept: application/json; indent=4" \
-    -H "Authorization: Token 0123456789abcdef0123456789abcdef01234567"
-  
-  sleep 20
-  
-  #
-  # backup database (overwriting our initial backup)
-  #
-  pg_dump -Z9 -f postgres_init.d/50_init.sql.gz --dbname=postgresql://netbox:J5brHrAXFLQSif0K@127.0.0.1:5432/netbox
-
-  #
-  # Migrate to 3.4 netbox to migrate secrets application
+  # spool up 3.4 netbox to migrate secrets application
   #
   podman-compose ${overrides} down -v
   podman volume prune -f
   git checkout bandwidth-3.4-2.5.3
   git pull --set-upstream origin bandwidth-3.4-2.5.3
   
-  podman-compose ${overrides} -f docker-compose.override.upgrade.yml build 
-  podman-compose ${overrides} -f docker-compose.override.upgrade.yml up --no-start
-  podman-compose ${overrides} -f docker-compose.override.upgrade.yml start
+  podman-compose ${overrides} build 
+  podman-compose ${overrides} up --detach
   
-  while ! curl -s -X GET http://localhost:8000 > /dev/null; do sleep 5; done
+  retval=1 
+  while [ $retval -ne 0 ]
+  do
+    echo "checking...."
+    sleep 5
+    podman-compose exec -T netbox curl -f http://localhost:8080/api/ > /dev/null 2>&1
+    retval=$?
+  done
+
+  #
+  # Create racktables_migration superuser account
+  #
+  podman-compose exec netbox \
+  /opt/netbox/venv/bin/python \
+  ./manage.py \
+  createsuperuser \
+  --username "${SUPERUSER_NAME}" \
+  --email "${SUPERUSER_EMAIL}" \
+  --noinput
+
+  # Create token for above account
+  cat<<EOF | podman-compose exec -T postgres psql --user netbox --dbname netbox
+INSERT INTO users_token (created, key, write_enabled, description, user_id)
+SELECT
+  NOW(),
+  '${SUPERUSER_API_TOKEN}',
+  True,
+  'racktables migration api key; delete me',
+  id
+FROM auth_user
+WHERE username = 'racktables_migration'
+ON CONFLICT(key) DO NOTHING;
+EOF
 
   # Finish netbox-secretstore to netbox-secrets migration
   echo "BEGIN;" > secretstore_cleanup.sql
 
-  podman exec -it netbox-docker_netbox_1 /opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py \
+  podman-compose exec -T netbox /opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py \
   sqlsequencereset netbox_secrets | \
   sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | \
   sed '1,/BEGIN;/d' | \
@@ -109,15 +93,15 @@ DROP TABLE IF EXISTS netbox_secretstore_userkey;
 COMMIT;
 SQL
 
-  podman cp secretstore_cleanup.sql netbox-docker_postgres_1:/secretstore_cleanup.sql
+  cat secretstore_cleanup.sql | podman-compose exec -T postgres psql --user netbox --dbname netbox
   rm secretstore_cleanup.sql
-
-  podman exec -it netbox-docker_postgres_1 psql --user netbox --dbname netbox -f /secretstore_cleanup.sql
 
   ##
   ## backup database (overwriting our initial backup)
   ##
-  pg_dump -Z9 -f postgres_init.d/50_init.sql.gz --dbname=postgresql://netbox:J5brHrAXFLQSif0K@127.0.0.1:5432/netbox
+  #pg_dump -Z9 -f postgres_init.d/50_init.sql.gz --dbname=postgresql://netbox:J5brHrAXFLQSif0K@127.0.0.1:5432/netbox
+  podman-compose exec -T postgres pg_dump --user netbox --dbname netbox \
+  | gzip > postgres_init.d/50_init.sql.gz
   
   #
   # Recreate netbox cluster with latest version
@@ -128,17 +112,26 @@ SQL
   git checkout bandwidth
   git pull --set-upstream origin bandwidth
 
-  podman-compose ${overrides} -f docker-compose.override.upgrade.yml build 
-  podman-compose ${overrides} -f docker-compose.override.upgrade.yml up --no-start
-  podman-compose ${overrides} -f docker-compose.override.upgrade.yml start
-  
-  while [ "$(podman container inspect netbox-docker_netbox_1 --format '{{ .State.Health.Status }}')" = 'starting' ]; do sleep 5; done
-  
+  podman-compose ${overrides} build 
+  podman-compose ${overrides} up --detach
+
+  retval=1 
+  while [ $retval -ne 0 ]
+  do
+    echo "checking...."
+    sleep 5
+    podman-compose exec -T netbox curl -f http://localhost:8080/api/ > /dev/null 2>&1
+    retval=$?
+  done
+
   #
   # backup upgraded voxbone database
   #
-  pg_dump -Z9 -f backups/voxbone_upgraded_backup.sql.gz --dbname=postgresql://netbox:J5brHrAXFLQSif0K@127.0.0.1:5432/netbox
-  rm postgres_init.d/50_init.sql.gz
-  
+  podman-compose exec -T postgres pg_dump --user netbox --dbname netbox \
+  | gzip > postgres_init.d/50_init.sql.gz
+
+  podman-compose ${overrides} down -v
+  podman volume prune -f
+
   exit
 }
