b3fa1f2e1f7bad6e21cb273a841a28365015ba2b
Michael Pereira <mpereira@bandwidth.com>
2023-06-12 12:35:39 -0400

    Remove admin password manipulation.

--- postgres_init.d/98_reset_admin_pw.sql
+++
@@ -1,6 +0,0 @@
-UPDATE
-  auth_user
-SET
-  password='pbkdf2_sha256$260000$5cQvjWzQCPFPYjAE6vJhc7$NL1kM6QOGre1SqBedaSVvgb/SM2LcaRfP6O+hN7SlK8=',
-  email='admin@foo.bar'
-WHERE username = 'admin';
--- postgres_init.d/99_reset_admin_token.sql
+++
@@ -1,6 +0,0 @@
-UPDATE
-  users_token
-SET
-  key = '0123456789abcdef0123456789abcdef01234567'
-WHERE
-  user_id = (SELECT id FROM auth_user WHERE username = 'admin')
