22d495014b4a448a9287eb139d1d19adc5825944
Michael Pereira <mpereira@mpereira-netbox.lab1.bwnet.us>
2023-06-21 15:37:32 +0000

    Remove proxyprefix

--- docker/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-from proxyprefix.wsgi import ReverseProxiedApp
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netbox.settings")
-
-#application = get_wsgi_application()
-application = ReverseProxiedApp(get_wsgi_application())
